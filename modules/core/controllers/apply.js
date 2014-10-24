6d2f6bab384a511ec9659717e175b75219b53ee0
Sander Sink <ss@fadeit.dk>
2014-10-24 16:46:18 +0200

    reworked form to load dynamically

---
+++ modules/core/controllers/apply.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('core').controller('ApplyController', ['$scope', function ($scope) {
+     angular.element('#formWrapper').load('http://danmind.ru/api/apply form#webform-client-form-1',function(){
+	angular.element('#webform-client-form-1').attr('action','http://danmind.ru/api/apply');
+     });
+
+}]);
