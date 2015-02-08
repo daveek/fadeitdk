f2a8e5c7bedf1682322acf674c7f46e2f21d7f92
Justas Azna <ja@fadeit.dk>
2015-02-08 22:20:14 +0100

    Remove the old site

--- modules/core/controllers/services.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('core').controller('ServicesController', ['$scope', '$log', 'AnimationService', function ($scope, $log, AnimationService) {
-    //load services
-    $scope.anim = AnimationService;
-}]);
