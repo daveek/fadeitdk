6c437159be7b833122aaa144cf44789d3624b74e
Sander Sink <ss@fadeit.dk>
2015-03-17 20:42:15 +0100

    changed expertise urls to use hyphen instead of underscore

--- src/app/expertise/expertise.controller.js
+++ src/app/expertise/expertise.controller.js
@@ -4,13 +4,7 @@ expertiseController.$inject = ['$stateParams', '$state'];
 function expertiseController($stateParams, $state) {
   var vm = this;
   vm.tech = $stateParams.tech;
-  $state.current.data.pageTitle = 'EXPERTISE_PAGE_TITLE_' + vm.tech.toUpperCase();
-  $state.current.data.pageDesc = 'EXPERTISE_' + vm.tech.toUpperCase();
-
-  
-  //vm.pageTitle = 'kuked!'+vm.tech;
-  //vm.htmlTitle = 'kuked'+vm.tech;
-  //TODO: optimize SEO skills
-  //title = TITLE_EXPERTISE_+upperCase(vm.tech);
+  $state.current.data.pageTitle = 'EXPERTISE_PAGE_TITLE_' + vm.tech.replace('-','_').toUpperCase();
+  $state.current.data.pageDesc = 'EXPERTISE_' + vm.tech.replace('-','_').toUpperCase();
 }
  
