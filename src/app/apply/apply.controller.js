f1f1b0988ebebe1e71e9ffe644dec036e3f7e078
Justas Azna <justas@reederz.com>
2016-09-30 16:16:28 +0200

    [#93] Add job application form directly into the app

--- src/app/apply/apply.controller.js
+++ src/app/apply/apply.controller.js
@@ -1,8 +1,38 @@
 angular.module('fadeit.apply').controller('ApplyController', applyController);
 
-applyController.$inject = [];
-function applyController(){
-  angular.element('#formWrapper').load('http://danmind.ru/api/apply form#webform-client-form-1', function(){
-      angular.element(document.querySelector('#webform-client-form-1')).attr('action','http://danmind.ru/api/apply');
-  });
+applyController.$inject = ['$scope'];
+function applyController($scope){
+  $scope.application = {};
+  $scope.apply = function (application) {
+    /* TODO:
+     - Make sure that email and name is present
+     - add success and failure feedback in frontend
+    */
+    var formData = new FormData();
+
+    // attach CV to form, if found
+    var fileSelect = document.getElementById('cv-upload');
+    if (fileSelect.files.length) {
+      var file = fileSelect.files[0];
+      formData.append('cv', file, file.name);
+    }
+
+    // attach the rest of the data to form
+    formData.append('name', application.name);
+    formData.append('email', application.email);
+    formData.append('education', application.education);
+    formData.append('message', application.message);
+
+    // send the application to backend
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/application', true);
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        alert('Success!');
+      } else {
+        alert('An error occurred!');
+      }
+    };
+    xhr.send(formData);
+  };
 }
